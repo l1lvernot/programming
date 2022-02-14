f7dc040b629f356c09380075f1a566988c212a26
ilavanin <ilya.vanin.05@icloud.com>
2022-02-14 23:04:46 +0200

    hw

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,20 +1,31 @@
 import java.util.Scanner;
 
 public class Ex3 {
+    static int shtoto(int n) {
+        int l = 0;
+        while (n != 0) {
+            l++;
+            n /= 10;
+        }
+        return l;
+    }
+
+    static int zzz(int n) {
+        int x = (int) (n / (Math.pow(10, shtoto(n) - 1)));
+        int x1 = n % 10;
+
+        n = n * 10 + ((x + x1) % 10);
+
+        x1 = n % 10;
+        n = n * 10 + ((x + x1) % 10);
+        return n;
+    }
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int num = s.nextInt();
 
-        for (int i = 0; i < num; i++) {
-            for (int x = 0; x < i + 1; x++) {
-                System.out.print('*');
-            }
-            for (int k = 1; k < num - i; k++) {
-                System.out.print('#');
-            }
-            System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(zzz(s.nextInt()));
         }
-
     }
 }
