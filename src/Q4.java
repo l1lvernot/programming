8fef438c4e86dd3644c4f16de0f9bdf4e98624ad
ilavanin <ilya.vanin.05@icloud.com>
2022-02-14 23:48:36 +0200

    hw

---
+++ src/Q4.java
@@ -0,0 +1,23 @@
+public class Q4 {
+    public static int shtoto(int x, int y, double z){
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        //1
+        int x=shtoto(3,7,8.2); //this one is working
+
+        //2
+        int b = 1;
+        int t = shtoto(b, b, 2.5); //this one is working
+
+        //3
+//        int c = test(7.8); //there is missing the first and the second argument
+
+        //4
+//        int m = test(4, 6.25, 9); //second argument can't be double it has to be int
+        if(shtoto(2,4,6.1)==1){
+            int k=7;
+        }
+    }
+}
